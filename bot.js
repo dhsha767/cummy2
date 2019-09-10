fd6c4ad9c5b94971198b0f8257459f6342a4331c
bmdyy <45366012+bmdyy@users.noreply.github.com>
2019-09-10 23:28:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,2 +1,29 @@
+/*
+  @title        Cummy 2.0
+  @description  Re-code of Dank Memes Cummy bot
+  @author       William Moody (@bmdyy#0068)
+  @date         10.09.2019
+*/
 
-setInterval(function(){http.get("http://cummy2.herokuapp.com");},300000); // make sure dyno doesn't sleep
+// --- --- --- --- --- --- --- ---
+
+const Discord = require('discord.js'); // discord api
+const PGClient = require('pg').Client; // postgresql
+const http = require('http'); // http requests (for the heartbeat)
+
+// --- --- --- --- --- --- --- ---
+
+const pgClient = new PGClient({ // init postgresql client
+  connectionString: process.env.DATABASE_URL,
+});
+pgClient.connect(); // connect to db
+const client = new Discord.Client(); // init discord api client
+
+// --- --- --- --- --- --- --- ---
+
+
+
+// --- --- --- --- --- --- --- ---
+
+client.login(process.env.BOT_TOKEN); // login to discord api
+setInterval(function(){http.get("http://cummy2.herokuapp.com");},300000); // make sure dyno doesn't fall asleep
