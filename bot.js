3f683e3381b5a65dc342eba7a30c3c3717ec249e
William Moody <45366012+bmdyy@users.noreply.github.com>
2019-09-12 09:34:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ function sendKarma(sender, reciever, amount) {
   if (amount <= 0) return;
   pgClient.query('select * from karma where uid='+sender.id+';').then((res) => {
     if (res.rows[0].karma < amount) return;
-    pgClient.query('update karma set karma=karma+1 where uid='+reciever.id+';update karma set karma=karma-1 where uid='+sender.id+';').then((res) =>
+    pgClient.query('update karma set karma=karma+'+amount+' where uid='+reciever.id+';update karma set karma=karma-'+amount+' where uid='+sender.id+';').then((res) =>
       console.log(res);
       return true;                                                                                                                                   {
     });
