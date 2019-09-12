f0815b7c0406c2d71677f33d4ff04c4e3318e872
William Moody <45366012+bmdyy@users.noreply.github.com>
2019-09-12 15:45:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,7 +115,7 @@ function updateTransactions(sender, reciever, amount) {
     .setColor(0xFFFF00)
     .setTitle('Past ' + TRANSACTIONS_MAX_COUNT + ' transactions');
   while (old_fields.length > TRANSACTIONS_MAX_COUNT - 1) { old_fields.pop(); }
-  embed.addField('_' + sender.username + '#' + sender.discriminator + '_ -> _' + reciever.username + '#' + reciever.discriminator + '_', amount + ' karma ['+getTimeStamp()+']');
+  embed.addField('_' + sender.username + '#' + sender.discriminator + '_ -> _' + reciever.username + '#' + reciever.discriminator + '_', '**' + amount + '** karma ['+getTimeStamp()+']');
   old_fields.forEach(field => {
     embed.addField(field.name, field.value, field.inline);
   });
