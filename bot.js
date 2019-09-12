bce909998df9b896d0059d1f9f346afbb2724571
William Moody <45366012+bmdyy@users.noreply.github.com>
2019-09-12 09:10:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -64,7 +64,7 @@ function findUser(string) { // searches for user by username#discrim and returns
   var userObj = null;
   if (string.match(USERSTRING_REGEX) == null) return;
   var args = string.split('#');
-  client.guilds.find(GUILD_ID).members.find(val => {
+  client.guilds.get(GUILD_ID).members.find(val => {
     return val['user'].username == args[0] && val['user'].discriminator == args[1];
   });
   return userObj;
